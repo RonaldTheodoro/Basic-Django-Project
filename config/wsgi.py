f3d796b1c249df30537034c6aafa40b46f922c2d
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-09-17 19:43:49 -0300

    use rawgit cdn or static

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,9 +9,16 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 from dj_static import Cling
+from decouple import config
 from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = Cling(get_wsgi_application())
+
+if config('DEBUG', default=False, cast=bool):
+    print('true')
+    application = Cling(get_wsgi_application())
+else:
+    print('false')
+    application = get_wsgi_application()
