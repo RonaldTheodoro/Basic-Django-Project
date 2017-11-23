978288fa18234f2748775c93287ae12834e987fb
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-23 21:20:14 -0200

    add staticfiles dirs

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 
 STATIC_URL = config('STATIC_URL', default='/static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
