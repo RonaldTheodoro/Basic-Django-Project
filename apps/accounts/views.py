2002a3443487c4b273d360d67e2ace2526e11ed4
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-23 22:19:46 -0200

    implement signup view

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,7 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('core:index')
+
+    else:
+        form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
