91c4f188270908e7ad62b5bafa3654d723ce2c7f
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-23 22:50:53 -0200

    use LogoutView

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 ]
