cd1dd745c1f507dbda12569f57e09a95183236d1
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-23 23:01:05 -0200

    use django Login cbv

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
+
 from . import views
+
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
 ]
