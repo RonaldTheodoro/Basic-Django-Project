b313dbefae2c4097e13a7f27443e69e999dddfe3
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-25 00:32:08 -0200

    implement my account view

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^my_account/$', views.UserUpdateView.as_view(), name='my_account'),
 ]
