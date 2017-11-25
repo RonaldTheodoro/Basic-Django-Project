d3dbadca86dab70eb0ff9bbe0e09d4e1a1ef07f3
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-24 22:41:19 -0200

    add reset password views

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     # Admin
@@ -8,4 +10,25 @@ urlpatterns = [
     url(r'^', include('apps.core.urls', namespace='core')),
     # Accounts app
     url(r'^accounts/', include('apps.accounts.urls', namespace='accounts')),
+
+    url(
+        r'reset/$',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    url(
+        r'reset/done/$',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+    url(
+        r'reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    url(
+        r'reset/complete/$',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
 ]
